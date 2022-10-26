52f88d39a6aca640047eb65f24ea4196fd58741a
jassi-singh-wig <jaskaran.singh@nationwide.co.uk>
2022-10-26 09:36:24 +0100

    day2

--- hello-world/src/server.py
+++ hello-world/src/server.py
@@ -1,17 +1,22 @@
+import logging
 from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
+logger = logging.getLogger('werkzeug')
+handler = logging.FileHandler('./logs/hw-app.log')
+logger.addHandler(handler)
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+   return "Hello World!"
 
 @app.route('/admin')
 def hello_admin():
-   return 'Hello Admin'
+   return "Hello Admin!"
 
 @app.route('/guest/<guest>')
 def hello_guest(guest):
-   return 'Hello %s as Guest' % guest
+   return "Hello %s as Guest" % guest
 
 @app.route('/user/<name>')
 def hello_user(name):
--- readme.md
+++ readme.md
@@ -1,6 +1,13 @@
 Hello World Challenge
 
-Day1: 24/10/2022
+main: 24/10/2022
 1. Sample Flask app
 2. ES, Kibana, Filebeat installed
 3. Container logs sent from the app + ES + Kibana + filebeat to ES using Filebeat --> Need to exclude all except Flask app logs
+
+day2: 25/10/2022
+1. Sample Flask app, updated to log in a local file
+2. Flask app logs saved in file sent to ES using Filebeat (used filestream instead of container)
+3. Updated to send logs from filebeat to Logstash, to extract & append some fields
+4. Saved search in kibana
+5. Created dashboard in kibana
